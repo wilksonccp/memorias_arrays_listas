0a09b568b4c82fa7444141e8aed705003d261336
Wilksonccp <wilkson.cc@gmail.com>
2024-06-18 20:30:41 -0300

    Criada aprimeira versão da calculadora - versão ruim

--- ExercicioParams/Program.cs
+++ ExercicioParams/Program.cs
@@ -8,24 +8,37 @@ namespace ExercicioParams
     {
         static void Main(string[] args)
         {
+            
+            int result = Calculator.Sum(8,3,5);
+            Console.WriteLine("Resultado: ", result);
             /*int result = Calculator.Sum( 30, 45, 89 );
             Console.WriteLine(result);*/
         }
     }
     internal class Calculator
     {
-        public static int Sum(params int[] nums)
+        public static int Sum(int n1, int n2)
         {
-            
-            
-            
-            /*int sum = 0;
+            return (n1 + n2);
+        }
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return (n1 + n2 + n3);
+        }
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return (n1 + n2 + n3 + n4);
+        }
+
+        /*public static int Sum(params int[] nums)
+        {
+            int sum = 0;
             for (int i = 0; i < nums.Length; i++)
             {
                 sum += nums[i];
             }
-            return sum;*/
-        }
+            return sum;
+        }*/
     }
 }
 

5f2234e6d092c7265b7d47fea9f01a819d05cb0d
Wilksonccp <wilkson.cc@gmail.com>
2024-08-14 17:23:43 -0300

    I finished a frist exercise

---
+++ ProblemaExemplo01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altSum = 0;
+            Console.Write("Quantas alturas serão inseridas: ");
+            int altQtd = int.Parse(Console.ReadLine());
+            double[] alt = new double[altQtd];
+            for (int i = 0; i < altQtd; i++)
+            {
+                alt[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            for (int i = 0; i < altQtd; i++)
+            {
+                altSum += alt[i];
+            }
+            double aver = altSum / altQtd;
+            Console.Write("AVERGE HEIGHT = " + aver.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+

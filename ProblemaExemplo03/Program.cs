1e53065f8760e3e84ce51df471a36dcdd3290f5b
Wilksonccp <wilkson.cc@gmail.com>
2024-09-05 08:09:33 -0300

    a new exercise has finished

--- ProblemaExemplo03/Program.cs
+++ ProblemaExemplo03/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace ProblemaExemplo03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int[,] matnumber = new int[number, number];
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.WriteLine($"Insert the number in line: {i} column: {j}");
+                    matnumber[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.Write("The main diagonal is: ");
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (i==j)
+                    {
+                    int showNumber = matnumber[i, j];
+                    Console.Write($" {showNumber}");
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.Write("The negatives number is: ");
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (matnumber[i, j] <0)
+                    {
+                    int showNumber = matnumber[i, j];
+                    Console.Write($" {showNumber}");
+                    }
+                }
+            }
+
+        }
+    }
+}
--- ProblemaExemplo03/obj/Debug/net8.0/ProblemaExemplo03.AssemblyInfo.cs
+++ ProblemaExemplo03/obj/Debug/net8.0/ProblemaExemplo03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProblemaExemplo03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4760def980aa85f5189a2d2c0ba82d3f305467aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdfed5040e7e1f6f91de74bedc32da87377c81e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProblemaExemplo03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProblemaExemplo03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
